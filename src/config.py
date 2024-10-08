26200f15649eeeb919d033d400449b968400375b
Aravind <aravind19112003@gmail.com>
2024-10-08 23:18:47 +0530

    Add requirements

--- src/config.py
+++ src/config.py
@@ -3,9 +3,11 @@ from dotenv import load_dotenv
 
 
 class Config:
+
     """
     Config class to access the necessary global variables.
     """
+    
     def __init__(self):
         load_dotenv()
         self.EMAIL_ID = os.getenv("EMAIL_ID")
--- src/notion_client.py
+++ src/notion_client.py
@@ -1,22 +1,27 @@
 import requests
 
-"""
-Client to interact with Notion API to fetch data from the integration
-"""
 class NotionClient:
+
+    """
+    Client to interact with Notion API to fetch data from the integration
+    """
+
     def __init__(self, notion_key):
         self.headers = {
             'Authorization': f"Bearer {notion_key}",
             'Content-Type': 'application/json',
             'Notion-Version': '2022-06-28'
         }
-    """
-    Fetch all pages from the Notion workspace.
-        
-    Returns:
-        list: List of all page objects.
-    """
+   
     def fetch_all_pages(self):
+
+        """
+        Fetch all pages from the Notion workspace.
+        
+        Returns:
+            list: List of all page objects.
+        """
+
         all_results = []
         next_cursor = None
         has_more = True
